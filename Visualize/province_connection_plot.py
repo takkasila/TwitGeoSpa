810df2d9c1204e8a3dbb588f1e9034a55b323ebc
takkasila <takkasila@outlook.com>
2016-07-04 11:18:14 +0700

    Plot two-way connection with total magnitude

--- Visualize/province_connection_plot.py
+++ Visualize/province_connection_plot.py
@@ -36,6 +36,33 @@ def writeConnectionLink(pvConnTable, pvcmDict, csvFile, isTwoWay):
                 , 'polyline' : genPolyLine(pvcmDict[pvDict[0]].outP, pvcmDict[link[0]].inP) if isTwoWay else genPolyLine(pvcmDict[pvDict[0]].polyCentroid, pvcmDict[link[0]].polyCentroid)
             })
 
+def writeConnectionLinkMagnitude(pvConnTable, pvcmDict, csvFile):
+    pvList = OrderedDict()
+    i = 0
+    for pv in pvConnTable.keys():
+        pvList[i] = pv
+        i += 1
+
+    connLineWriter = csv.DictWriter(open(csvFile, 'wb'), delimiter = ';', fieldnames=['from','to','polyline','weight'])
+    connLineWriter.writeheader()
+    for x in range(len(pvConnTable)):
+        for y in range(x, len(pvConnTable)):
+            magnitude = pvConnTable[pvList[x]][pvList[y]] - pvConnTable[pvList[y]][pvList[x]]
+
+            if magnitude > 0:
+                fromPv = pvList[x]
+                toPv = pvList[y]
+            else:
+                fromPv = pvList[y]
+                toPv = pvList[x]
+
+            connLineWriter.writerow({
+                'from' : fromPv
+                , 'to' : toPv
+                , 'weight' : abs(magnitude)
+                , 'polyline' : genPolyLine(pvcmDict[fromPv].polyCentroid, pvcmDict[toPv].polyCentroid)
+            })
+
 def genPolyLine(p1, p2):
     return 'LINESTRING(' + str(p1.x) + ' ' + str(p1.y) + ', ' + str(p2.x) + ' ' + str(p2.y) +')'
 
@@ -45,9 +72,16 @@ if __name__ == '__main__':
         exit()
 
     isTwoWay = raw_input('Is two way connection? (y/n): ').lower()
+    isTwoWay = True if isTwoWay == 'y' else False
+    isMagnitude = False
+    if isTwoWay:
+        isMagnitude = raw_input('Is magnitude? (y/n): ').lower()
+        isMagnitude = True if isMagnitude == 'y' else False
 
     pvcmHolder = ProvinceCMPointHolder(shapefile.Reader(sys.argv[1]), abbrCsv = '../Province/Province from Wiki Html table to CSV/ThailandProvinces_abbr.csv')
     pvConnTable = readConnectionTable(sys.argv[2])
 
-    isTwoWay = True if isTwoWay == 'y' else False
-    writeConnectionLink(pvConnTable, pvcmHolder.pvcmDict, sys.argv[3], isTwoWay)
+    if isMagnitude == False:
+        writeConnectionLink(pvConnTable, pvcmHolder.pvcmDict, sys.argv[3], isTwoWay)
+    else:
+        writeConnectionLinkMagnitude(pvConnTable, pvcmHolder.pvcmDict, sys.argv[3])
