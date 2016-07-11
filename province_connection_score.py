caf3905997e755359b73d6c0b8f539c54e1d2fc8
takkasila <takkasila@outlook.com>
2016-07-11 16:45:47 +0700

    Plot connection score

--- province_connection_score.py
+++ province_connection_score.py
@@ -1,16 +1,18 @@
 from province_connection_table import *
+from province_point import *
 
 if __name__ == '__main__':
-    if(len(sys.argv) < 3):
-        print 'Please insert processed twitdata .csv and output file name.'
+    if(len(sys.argv) < 4):
+        print 'Please insert shapefile, processed twitdata .csv and output file name.'
         exit()
 
+    pvPHolder = ProvinceCMPointHolder(shapefile.Reader(sys.argv[1]), abbrCsv = './Province/Province from Wiki Html table to CSV/ThailandProvinces_abbr.csv')
     provinceHolder = ProvinceHolder()
     provinceTable = ProvinceTable(provinceHolder.provinces)
-    provinceHolder.readDataFromCsv(csvFile = sys.argv[1])
+    provinceHolder.readDataFromCsv(csvFile = sys.argv[2])
     provinceTable.createTableOfCommonUID()
 
-    scoreWriter = csv.DictWriter(open(sys.argv[2], 'wb'), delimiter = ',', fieldnames=['name', 'score', 'Number of UID'])
+    scoreWriter = csv.DictWriter(open(sys.argv[3], 'wb'), delimiter = ',', fieldnames=['name', 'score', 'Number of UID', 'point'])
     scoreWriter.writeheader()
     for y in range(len(provinceTable.table)):
         totalConn = 0.0
@@ -20,8 +22,12 @@ if __name__ == '__main__':
             score = totalConn / len(provinceTable.provinces[y].uidList)
         except:
             score = 0
+
+        name = provinceTable.provinces[y].name
+        point = pvPHolder.pvcmDict[name].polyCentroid
         scoreWriter.writerow({
-            'name' : provinceTable.provinces[y].name
+            'name' : name
             , 'score' : score
             , 'Number of UID' : len(provinceTable.provinces[y].uidList)
+            , 'point' : 'POINT('+str(point.x)+' '+str(point.y)+')'
         })
